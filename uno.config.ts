d274624c24cebca64c6a0ae01d3b747a47990f2e
te <thrzl@icloud.com>
2026-01-09 12:15:06 -0500

    ui: change link style

--- uno.config.ts
+++ uno.config.ts
@@ -1,3 +1,12 @@
 import { defineConfig } from "unocss";
 
-export default defineConfig({});
+export default defineConfig({
+  theme: {
+    colors: {
+      dominant: "var(--dominant, #e9e7d7)",
+      "text-color": "var(--textColor, black)",
+      "secondary-text-color": "var(--secondaryTextColor, white)",
+      accent: "var(--accent, blue)",
+    },
+  },
+});
