cf94f3ffa1ea062009070a5e2961eda7b59aba1f
te <thrzl@icloud.com>
2026-01-09 11:57:23 -0500

    feat: add mdx support + dayjs

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
-export default defineConfig({ integrations: [UnoCSS()] });
+export default defineConfig({ integrations: [UnoCSS(), mdx()] });
