bd41bcf4764bd6e4b976052355084eedea919b32
te <thrzl@icloud.com>
2026-01-12 14:48:55 -0500

    feat: sort projects by date

--- src/content.config.ts
+++ src/content.config.ts
@@ -10,6 +10,11 @@ export function toSlug(text: string) {
     .replace(/^-+|-+$/g, "");
 }
 
+function dateToEST(date: Date, ctx: z.RefinementCtx) {
+  const estOffsetMs = -5 * 60 * 60 * 1000; // est offset (-5 hours)
+  return new Date(date.getTime() - estOffsetMs);
+}
+
 const blog = defineCollection({
   loader: glob({ base: "./src/posts", pattern: "**/*.{md,mdx}" }),
   schema: z
@@ -17,10 +22,7 @@ const blog = defineCollection({
       title: z.string(),
       slug: z.string().optional(),
       description: z.string(),
-      date: z.date().transform((date, ctx) => {
-        const estOffsetMs = -5 * 60 * 60 * 1000; // est offset (-5 hours)
-        return new Date(date.getTime() - estOffsetMs);
-      }),
+      date: z.date().transform(dateToEST),
       tags: z.array(z.string()).default([]),
       image: z.string().optional(),
     })
@@ -42,13 +44,22 @@ const projects = defineCollection({
       description: z.string(),
       tags: z.array(z.string()).default([]),
       image: z.string().optional(),
+      startDate: z.date().transform(dateToEST),
+      endDate: z.date().transform(dateToEST).optional(),
     })
     .transform((object) => {
       const slug = object.slug || toSlug(object.title);
       const url = object.url || `/projects/${slug}`;
+      const startYear = object.startDate.getFullYear();
+      const endYear = object.endDate?.getFullYear();
+      const date =
+        startYear === endYear || startYear === new Date().getFullYear()
+          ? startYear
+          : `${startYear} – ${endYear ?? "now"}`;
       return {
         ...object,
         slug,
+        date,
         url,
       };
     }),
