91904754aa3ad51594f9be0dbc9b385dc8c971ac
te <thrzl@icloud.com>
2026-01-11 21:55:09 -0500

    fix: timezone-aware post dates

--- src/content.config.ts
+++ src/content.config.ts
@@ -17,14 +17,19 @@ const blog = defineCollection({
       title: z.string(),
       slug: z.string().optional(),
       description: z.string(),
-      date: z.date(),
+      date: z.date().transform((date, ctx) => {
+        const estOffsetMs = -5 * 60 * 60 * 1000; // est offset (-5 hours)
+        return new Date(date.getTime() - estOffsetMs);
+      }),
       tags: z.array(z.string()).default([]),
       image: z.string().optional(),
     })
-    .transform((object) => ({
-      ...object,
-      slug: object.slug || toSlug(object.title),
-    })),
+    .transform((object) => {
+      return {
+        ...object,
+        slug: object.slug || toSlug(object.title),
+      };
+    }),
 });
 
 const projects = defineCollection({
